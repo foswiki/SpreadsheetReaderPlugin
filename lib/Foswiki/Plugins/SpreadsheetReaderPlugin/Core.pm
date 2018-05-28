a4ecac82572492bdbd9d2f33ed24c86679c70c8d
MichaelDaum <daum@michaeldaumconsulting.com>
2018-05-28 12:32:23 +0200

    Item14691: fix encoding problems
    as well as
    
    - implement row and column interval syntax
    - implement alphabetic column selector
    - implement strip parameter
    - implement headername_include and headername_exclude filtering

--- lib/Foswiki/Plugins/SpreadsheetReaderPlugin/Core.pm
+++ lib/Foswiki/Plugins/SpreadsheetReaderPlugin/Core.pm
@@ -21,15 +21,14 @@ use Spreadsheet::Read ();
 use Foswiki::Func ();
 #use Data::Dump qw(dump);
 use Error qw(:try);
+use Encode ();
 
-use constant TRACE => 0; # toggle me
+use constant TRACE => 0;    # toggle me
 
 sub new {
   my $class = shift;
 
-  my $this = bless({
-    @_
-  }, $class);
+  my $this = bless({@_}, $class);
 
   return $this;
 }
@@ -44,15 +43,15 @@ sub SPREADSHEET {
 
   my ($theWeb, $theTopic) = Foswiki::Func::normalizeWebTopicName($params->{web} || $web, $params->{topic} || $topic);
 
-  return _inlinError("access denied") unless
-        Foswiki::Func::checkAccessPermission("VIEW", $session->{user}, undef, $theTopic, $theWeb);
+  return _inlinError("access denied")
+    unless Foswiki::Func::checkAccessPermission("VIEW", $session->{user}, undef, $theTopic, $theWeb);
 
   return _inlineError("topic not found") unless Foswiki::Func::topicExists($theWeb, $theTopic);
 
   my ($fileName) = Foswiki::Func::sanitizeAttachmentName($attachment);
   return _inlineError("attachment not found") unless Foswiki::Func::attachmentExists($theWeb, $theTopic, $fileName);
 
-  my $filePath = $Foswiki::cfg{PubDir} . '/' . $web . '/' . $topic . '/' . $fileName;
+  my $filePath = $Foswiki::cfg{PubDir} . '/' . $theWeb . '/' . $theTopic . '/' . $fileName;
   _writeDebug("filePath=$filePath");
 
   return _inlineError("file not found") unless -e $filePath;
@@ -70,12 +69,12 @@ sub SPREADSHEET {
   my $book;
   try {
     $book = Spreadsheet::Read->new($filePath, $opts);
-  } catch Error with {
+  }
+  catch Error with {
     $error = shift;
     $error =~ s/ at .*$//;
   };
   return _inlineError($error) if defined $error;
-
 
   my $theSheets = $params->{sheets} || $params->{sheet};
   my $maxSheets = $book->sheets;
@@ -89,7 +88,7 @@ sub SPREADSHEET {
   my @selectedSheets = ();
   if (defined $theSheets) {
     if ($theSheets eq 'all') {
-      push @selectedSheets, $_ foreach 1..$maxSheets;
+      push @selectedSheets, $_ foreach 1 .. $maxSheets;
     } else {
       @selectedSheets = split(/\s*,\s*/, $theSheets);
     }
@@ -97,21 +96,14 @@ sub SPREADSHEET {
     push @selectedSheets, 1;
   }
 
-  my @selectedRows = ();
-  my $theRows = $params->{rows} || 'all';
-  if ($theRows ne "all") {
-    @selectedRows = split(/\s*,\s*/, $theRows);
-  }
-
-  my @selectedCols = ();
-  my $theCols = $params->{cols} || 'all';
-  if ($theCols ne "all") {
-    @selectedCols = split(/\s*,\s*/, $theCols);
-  }
-  
+  my $theRows = $params->{rows} // 'all';
+  my $theCols = $params->{cols} // 'all';
+  my $theStrip = Foswiki::Func::isTrue($params->{strip}, 1);
 
   #_writeDebug("selectedSheets=@selectedSheets");
   my $class = $params->{class} // "foswikiTable";
+
+  my @headers = ();
 
   my @result = ();
   foreach my $sheetId (@selectedSheets) {
@@ -120,39 +112,40 @@ sub SPREADSHEET {
     my $maxCols = $sheet->maxcol;
     _writeDebug("reading sheet $sheetId, maxRows=$maxRows, maxCols=$maxCols");
 
-    if ($theRows eq "all") {
-      @selectedRows = ();
-      push @selectedRows, $_ foreach 1..$maxRows;
-    }
-
-    if ($theCols eq "all") {
-      @selectedCols = ();
-      push @selectedCols, $_ foreach 1..$maxCols;
-    }
+    my @selectedRows = _expandRange($theRows, $maxRows);
+    my @selectedCols = _expandRange($theCols, $maxCols);
 
     my $isHeader = 1;
     push @result, "<table class='$class'><thead>";
-    my $index = 1;
+    my $rowIndex = 0;
     foreach my $row (@selectedRows) {
       my @cells = ();
       if ($theShowIndex) {
         if ($isHeader) {
           push @cells, "<th>#</th>";
         } else {
-          push @cells, "<th>$index</th>";
-          $index++;
+          push @cells, "<th>$rowIndex</th>";
         }
       }
+
+      my $colIndex = 0;
+      my $enabled = 1;
       foreach my $col (@selectedCols) {
-	my $key = $book->cr2cell($col, $row);
-	my $val = $sheet->cell($key);
-	my $rawVal = $sheet->cell($col, $row) || '';
-	my $attr = $sheet->attr($key);
-	my $type = $attr->{type} || '';
-	_writeDebug("$key=$val, raw=$rawVal, type=$type");
-	#_writeDebug(dump($attr));
-	my $align = $attr->{halign};	
-	$align = "right" if !defined $align && $type eq "numeric";
+        my $key = $book->cr2cell($col, $row);
+        my $attr = $sheet->attr($key);
+        my $enc = $attr->{enc};
+
+        my $val = $sheet->cell($key);
+        $val = Encode::decode($enc, $val) if defined $enc;
+        $val =~ s/^\s+|\s+//g if $theStrip;
+
+        my $type = $attr->{type} || '';
+        #_writeDebug(dump($attr));
+        my $align = $attr->{halign};
+        $align = "right" if !defined $align && $type eq "numeric";
+
+        _writeDebug("$key=$val, type=$type, enc=" . ($enc // 'undef').", align=$align");
+
         my $styles = "";
         if ($theShowAttrs) {
           my @styles = ();
@@ -161,13 +154,33 @@ sub SPREADSHEET {
           push @styles, "font-weight:bold" if $attr->{bold};
           push @styles, "font-style:italic" if $attr->{italic};
           push @styles, "text-align:$align" if $align;
-          $styles = @styles?"style='".join(";", @styles)."'":"";
+          $styles = @styles ? "style='" . join(";", @styles) . "'" : "";
         }
-	push @cells, $isHeader?"<th $styles> $val </th>":"<td $styles> $val </td>";
+
+        if ($isHeader) {
+          my $header = $val;
+          $header =~ s/^\s+|\s+$//g unless $theStrip;
+          $header =~ s/[^A-Za-z0-9]//g;    # clean attr
+          push @headers, $header;
+        } else {
+          my $header = $headers[$colIndex++];
+          my $include = $params->{$header . "_include"};
+          my $exclude = $params->{$header . "_exclude"};
+          $enabled = 0 if defined($include) && $val !~ /$include/i;
+          $enabled = 0 if defined($exclude) && $val =~ /$exclude/i;
+          last unless $enabled;
+        }
+
+        push @cells, $isHeader ? "<th $styles> $val </th>" : "<td $styles> $val </td>";
       }
-      my $line = "<tr>". join("", @cells). "</tr>";
-      #_writeDebug("line=$line");
-      push @result, $line;
+
+      if ($enabled) {
+        my $line = "<tr>" . join("", @cells) . "</tr>";
+        #_writeDebug("line=$line");
+        push @result, $line;
+        $rowIndex++;
+      }
+
       push @result, "</thead><tbody>" if $isHeader;
       $isHeader = 0;
     }
@@ -175,6 +188,62 @@ sub SPREADSHEET {
   push @result, "</tbody></table>";
 
   return join("\n", @result);
+}
+
+sub _expandRange {
+  my ($range, $max) = @_;
+
+  $range =~ s/^\s+|\s+$//g;
+
+  my @result = ();
+
+  foreach my $item (split(/\s*,\s*/, $range)) {
+    my $from = 1;
+    my $to = $max;
+    if ($item eq "all") {
+    } elsif ($item =~ /^([A-Z]+)\s*\-$/) {
+      $from = _label2col($1);
+    } elsif ($item =~ /^\-\s*([A-Z]+)$/) {
+      $to = _label2col($1);
+    } elsif ($item =~ /^([A-Z]+)\s*\-\s*([A-Z]+)$/) {
+      $from = _label2col($1);
+      $to = _label2col($2);
+    } elsif ($item =~ /^([A-Z]+)$/) {
+      $from = $to = _label2col($1);
+    } elsif ($item =~ /^(\d+)\s*\-$/) {
+      $from = $1;
+    } elsif ($item =~ /^\-\s*(\d+)$/) {
+      $to = $1;
+    } elsif ($item =~ /^(\d+)\s*\-\s*(\d+)$/) {
+      $from = $1;
+      $to = $2;
+    } elsif ($item =~ /^(\d+)$/) {
+      $from = $to = $1;
+    }
+    next if $from > $max;
+    $to = $max if $to > $max;
+    push @result, $_ foreach $from .. $to;
+  }
+
+  #print STDERR "range=$range expands to ".join(", ", @result)."\n";
+
+  return @result;
+}
+
+sub _label2col {
+  my ($label) = @_;
+
+  $label = uc($label);
+  return unless $label =~ /^[A-Z]+$/;
+
+  my $col = 0;
+  while ($label =~ /([A-Z])/g) {
+    $col = 26 * $col + 1 + ord($1) - ord("A");
+  }
+
+  #print STDERR "label=$label, col=$col\n";
+
+  return $col;
 }
 
 sub _inlineError {
@@ -186,6 +255,5 @@ sub _writeDebug {
   #Foswiki::Func::writeDebug("SpreadsheetReaderPlugin::Core - $_[0]");
   print STDERR "SpreadsheetReaderPlugin::Core - $_[0]\n";
 }
-
 
 1;
